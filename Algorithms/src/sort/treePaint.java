dfe0c372d7314ed11fb641623f9be2c6dee70874
coolAutumn <autumn3376@gmail.com>
2016-06-19 22:26:03 +0800

    heapsort complete Page 43

---
+++ Algorithms/src/sort/treePaint.java
@@ -0,0 +1,20 @@
+package Sort;
+
+/**
+ * Created by coolAutumn on 6/19/16.
+ */
+public class treePaint {
+    //边进行深度遍历边打印出来
+    public static void paintTree(int[] array){
+        int level=0;
+        int numberHasPainted=0;
+        int numberOfLevel= (int) Math.pow(2,level);
+        for(int i=0;i<array.length;i++){
+            for(int j=1;j<numberOfLevel;j++){
+                System.out.println();
+                numberHasPainted++;
+            }
+            numberOfLevel=(int)Math.pow(2,level);
+        }
+    }
+}
